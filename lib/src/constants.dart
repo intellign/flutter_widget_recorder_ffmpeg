204932d099dea8e1154e68fdd3eaf812077ced08
intellign <ismailarashad@aol.com>
2024-11-19 00:34:06 -0500

    Update constants.dart

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,5 +1,6 @@
+String basePath = '/data/user/0/com.example.example2/cache/';
+
 class Constants {
-  static String basePath = '/data/user/0/com.example.example2/cache/';
   static String imagesPath = basePath + '%0d.png';
   static String gifOutputPath = basePath + 'output.gif';
   static String videoOutputPath = basePath + 'output.mp4';
